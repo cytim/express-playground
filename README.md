d587b941c8a16fb63f98f1b463af46512e55c419
Tim Wong <cytimo@gmail.com>
2018-09-28 09:54:18 +0800

    Add the dummy log API

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 > The playground for Express.
 
+
 ## APIs
 
-### `GET /_health`
+
+### GET `/_health`
 
 Return HTTP status code 200.
 
-### `GET /load`
+
+### GET `/load`
 
 Spawn a new thread to execute the heavy job asynchronously.
 
@@ -17,4 +20,20 @@ Spawn a new thread to execute the heavy job asynchronously.
 | Key | Type | Constraint | Description |
 |-----|------|------------|-------------|
 | `x` | Number | Min: 1; Max: 5 | The factor to iterate the heavy job. "1" means 10^1 times, "2" means 10^2 times, etc. |
+
+
+### POST `/logs/dummy`
+
+Create a new dummy log.
+
+*Body*
+
+| Key | Type | Constraint | Description |
+|-----|------|------------|-------------|
+| `message` | String | | The log message. |
+
+
+### GET `/logs/dummy`
+
+Get all dummy logs.
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express')
 const { fork } = require('child_process')
+const { mkdirSync, appendFile, readFile } = require('fs')
+const bodyParser = require('body-parser')
+const express = require('express')
 const pino = require('pino')
 
 const log = pino({
@@ -8,7 +10,26 @@ const log = pino({
   prettyPrint: process.env.LOG_PRETTY_PRINT === 'true',
 })
 
+
+// PREPARE THE APPLICATION ENVIRONMENT
+
+try {
+  mkdirSync('log')
+  log.info('Created the "log" directory')
+} catch(e) {
+  if (e.code === 'EEXIST') {
+    log.info('Skip creating the "log" directory: directory already exists')
+  } else {
+    log.error(`Failed to create the "log" directory: ${e.message}`)
+  }
+}
+
+
+// SETUP THE SERVER
+
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Express Playground'))
 
@@ -20,6 +41,40 @@ app.get('/load', (req, res) => {
   res.json({ acknowledged: true })
 })
 
+app.post('/logs/dummy', (req, res) => {
+  log.info('Received a request to write some dummy logs')
+  const now = new Date()
+  let { message } = req.body
+
+  if (!message || typeof message !== 'string') {
+    res.status(400).json({ error: '"message" must be a non-empty string' })
+    return
+  }
+
+  message = now.toISOString() + ' - ' + message
+  appendFile('log/dummy.log', `${message}\n`, (err) => {
+    if (err) {
+      res.status(500).json({ error: err.message })
+    } else {
+      res.json({ data: message })
+    }
+  })
+})
+
+app.get('/logs/dummy', (req, res) => {
+  log.info('Received a request to get the dummy logs')
+  readFile('log/dummy.log', (err, content) => {
+    if (err) {
+      res.json({ data: [] })
+    } else {
+      res.json({ data: content.toString().replace(/\n+$/, '').split('\n') })
+    }
+  })
+})
+
+
+// START THE SERVER
+
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server listening on port ${port}...`))
+app.listen(port, () => log.info(`Server listening on port ${port}...`))
 
